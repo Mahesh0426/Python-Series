2b8217d5620c153ddd0b469f0c8b6b975405b6e5
Mahesh792 <mawhesjung143@gmail.com>
2025-04-10 12:44:03 +1000

    this project completed with sqlite db

--- 10_Sqllite_database/youtube_manager_LiteDB.py
+++ 10_Sqllite_database/youtube_manager_LiteDB.py
@@ -13,8 +13,23 @@ CREATE TABLE IF NOT EXISTS videos (
                )
 ''')
 
-list_videos():
+def list_videos():
+    cursor.execute("SELECT * FROM videos")
+    for row in cursor.fetchall():
+        print(row)
 
+def  add_video(name,time):
+    cursor.execute("INSERT INTO videos(name , time) VALUES(?, ?)", (name, time))
+    connection.commit()
+    
+
+def update_video(video_id, new_name, new_time):
+    cursor.execute("UPDATE videos SET name = ?, time = ? WHERE id = ?", (new_name, new_time, video_id))
+    connection.commit()
+
+def delete_video(video_id):
+    cursor.execute("DELETE FROM videos WHERE id = ?", (video_id,))
+    connection.commit()
 
       
 
@@ -30,6 +45,26 @@ def main():
 
         if choice == '1':
             list_videos()
+        elif choice == '2':
+            name = input("Enter video title: ")
+            time = input("Enter video time: ")
+            add_video(name, time)
+        elif choice == '3':
+            video_id =  input("Enter video id to update: ")
+            name = input("Enter video title to update: ")
+            time = input("Enter video time to update: ")
+            update_video(video_id, name, time)
+        elif choice == '4':
+            video_id = input("Enter video id to delete:")
+            delete_video(video_id)
+        elif choice == '5':
+            break
+        else:
+            print("Invalid choice.")
+            
+    connection.close()
+
+        
         
 if __name__ == "__main__":
     main()
