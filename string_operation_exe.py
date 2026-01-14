8c881bf1e17a0bf7844b42413d29187165f47f4d
Mahesh792 <mawhesjung143@gmail.com>
2026-01-14 23:49:17 +1100

    sqlite db connect in python

--- string_operation_exe.py
+++ string_operation_exe.py
@@ -1,43 +1,70 @@
-#Write a program that takes two strings:
-# Join them with a space and print the full name.
+# 1.Clean and Format User Input
+raw_name = "   maHeSh kunWar   "
+clean_name = raw_name.strip()
+print(clean_name.title())
 
-firstName = "Mahesh"
-lastName = "Kunwar"
-fullName = firstName + " " + lastName
-print(fullName)
-print(f"{firstName} {lastName}") # Using f-string for string interpolation
-print("{} {}".format(firstName, lastName)) # Using format method
-print(firstName, lastName) # Using comma in print function
+# 2. Extract Domain From Email
+email = "  student123@university.edu  "
+clean_email = email.strip()
+print(clean_email.split("@")[1]) # this extracts the word after @
 
-#lowrcase
-text = "PYTHON IS FUN"
-print(text.lower())
+# 3. Count Words in a Sentence
+sentence = "Python is great and Python is powerful"
+word_to_count = "Python"
+count = sentence.count(word_to_count)
+print(count)
+# print(sentence.split())  
+print(len(sentence.split()))  # total words in the sentence
 
+#4. Mask a Phone Number | Replace the first 8 digits with *
+phone = "  +61 456 789 123  "
+masked_phone = phone.strip().replace(" ", "")  # Remove spaces
+print(masked_phone.replace(masked_phone[:9], "********"))
 
-msg = "    Welcome to Python  "
-print(msg.strip())  # Removes leading and trailing whitespace
-print(msg.lstrip()) # Removes leading whitespace
-print(msg.rstrip()) # Removes trailing whitespace
+# 5. Create a Slug for URLs
+title = "  Learn Python Programming in 2025  "
+slug = title.lower().strip().replace(" ", "-")
+print(slug)
 
-line = "I love JavaScript"
-print(line.replace("JavaScript","Pyhton"))
-
-programming_languages = "Python, Java, C++, JavaScript"
-print(programming_languages.split(","))
-print(programming_languages.join("|"))
-
-title = "Welcome to Masala Chai House"
-print(title.find("o"))
-count = title.count("o")
-print(f"Number of 'o' in the title is: {count}")
+#6 Merge a List to Create a Sentence
+words = ["python", "makes", "coding", "fun"]
+sentence = " ".join(words).upper()
+print(sentence)
 
 
-items = ["Keyboard", "Mouse", "Monitor"]
-print(" | ".join(items))
+# 7. Check If a Word Exists
+text = "Welcome to the Python Developer Community"
+word_to_check = "Developer"
+print(word_to_check in text)
 
-msg = "i am learning Python"
-print(msg.capitalize())
-print(msg.title())
-print(len(msg))
-print(msg.startswith("i am"))
-print(msg.endswith("Python"))
+# 8. Reverse Each Word  | output: I evol gninrael nohtyP | reverse each word
+line = "I love learning Python"
+# print(" ".join(word[::-1] for word in line.split()))
+words = line.split()
+print(words)
+reversed_words = [word[::-1] for word in words]
+print(reversed_words)
+result = " ".join(reversed_words)
+print(result)
+
+#9. Find and Replace Multiple Words
+msg = "I like Java and Java is popular"
+replace_words = msg.replace("Java", "Python")
+print(replace_words)
+print(replace_words.count("Python"))
+
+# 10. Format Billing Information
+name = "Mahesh"
+product = "Laptop"
+price = 1450.75
+result = f"{name} purchased {product} for ${price:.2f}"
+print(result)
+
+
+#11. Combine Three Strings Smartly
+first = "Hello"
+second = "World"
+third = "from Python"
+result = " , ".join([first, second, third])
+print(result)
+
--- string_operations.py
+++ string_operations.py
@@ -14,48 +14,61 @@ str4 = "hello world"
 result = str4.upper()
 print(result)
 
-#strip
+#title
+str_title = "hello world from python"
+result = str_title.title() # Convert to first letter of each word to uppercase
+print(result) #Hello World From Python
+
+#capitalize
+str_capitalize = "hello rOCKETS"
+result = str_capitalize.capitalize() # Convert first letter to uppercase and rest to lowercase
+print(result) #Hello rockets
+
+#strip | remove left and right spaces but not in middle 
 str5 = "   hello world   "
 print(str5.strip())  # "hello world" (removes leading & trailing spaces)
 print(str5.lstrip()) # "hello world  " (removes only left spaces)
 print(str5.rstrip()) # "  hello world" (removes only right spaces)
 
-
-
-#replace
+#replace | replace a part of string
 str6 = "Hello World"
 result = str6.replace("World", "Universe")
-print(result)
+print(result) #Hello Universe
+
+#2 replace  | replace spaces in middle
+phone = "+61 456 789 123"
+result = phone.replace(" ", "")
+print(result) #+61456789123
 
 #split
 languages = "Python, C-sharp, JavaScript, TypeScript"
 result = languages.split(", ")
-print(result)
+print(result) #["Python", "C-sharp", "JavaScript", "TypeScript"]
 
 #find
 backend = "Welcome to Python"
 result = backend.find("Python")
-print(result)
+print(result) #7
 
 #count
 countStrig = "Hello World"
 result = countStrig.count("e")
-print(result)
+print(result) #2
 
 #format
 name = "Mahesh"
 language = 3
 result = "My name is {} and I know {} programming languages"
-print(result.format(name, language))
+print(result.format(name, language)) #My name is Mahesh and I know 3 programming languages
 
 #join
 languages = [ "Python", "C-sharp", "JavaScript", "Ruby" ]
 result = ", ".join(languages) # Join elements of a list with a comma and space
-print(result)
+print(result) #Python, C-sharp, JavaScript, Ruby
 
 #length
 lengthStr = "Hello World"
-print(len(lengthStr))
+print(len(lengthStr)) #11
 
 #for in 
 # day = "Sunday"
@@ -68,8 +81,8 @@ print('It\'s a beautiful day')  #Using escape character for single quote (\')
 
 # print as it is using r
 print(r"c:\user\pwd") #prints c:\user\pwd
-print(r"python/nlanguage")
+print(r"python/nlanguage") #python/nlanguage
 
 #in 
 chaiName = "Masala Chai"    
-print("Masala" in chaiName) 
+print("Masala" in chaiName) #True
