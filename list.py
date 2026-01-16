3b8e178f57e2cef4858f9cdf33b33e3e7a19bf56
Mahesh792 <mawhesjung143@gmail.com>
2026-01-16 16:44:53 +1100

    api handling

--- list.py
+++ list.py
@@ -1,7 +1,7 @@
 # numbers = [1, 2, 3, 4, 5]
 mixed = ["hello", 42, 3.14, True]
 fruits = ["apple", "banana", "cherry","orange"]
-print(fruits)
+# print(fruits)
 # print(fruits[0])  # First element
 # print(fruits[-1])  # Last element
 
@@ -12,24 +12,24 @@ print(fruits)
 
 #Modifying Elements
 fruits[0]= "blueberries"
-print(fruits)
+# print(fruits)
 # fruits[1:2]= "grapes" 
 # print(fruits)
-fruits[1:1]= ["grapes"]
-print(fruits)
+fruits[1:2]= ["grapes"]
+# print(fruits)
 fruits[1:2]= []
-print(fruits)
+# print(fruits)
 
-#insert (Add at a specific index)
+#insert (Add at a specific index) | list.insert(index, value)
 fruits.insert(4,"Kiwi")
 # print(fruits)
 
-#append (Add at the end)
+#append (Add at the end) | list.append(value)
 fruits.append("Mango")
 # print(fruits)
 
 # remove (Remove by value)
-# fruits.remove("banana")
+fruits.remove("orange")
 # print(fruits)
 
 # pop (Remove by index, default is the last one)
@@ -52,36 +52,36 @@ Frameworks = ["numpy","pandas","tensorflow","pytorch","Scikit-learn"]
 
 
 #Searching in a List
-# numbers = [0,1,3,4,5,6,7,8,9,10]
-# print(3 in numbers) 
-# print(numbers.index(4))
+numbers = [0,1,3,1,4,5,6,7,8,9,10]
+# print(3 in numbers)  # check if 3 is in the list | return boolean
+# print(numbers.index(4))    # give the index of the value
 # print(numbers.count(1)) 
  #This counts how many times the value 1 appears in the list numbers.
 
 
 # Sorting a List
-# number = [5, 3, 1, 4, 2]
-# number.sort()
+number = [5, 3, 1, 4, 2]
+number.sort()
 # print(number) 
  # Output: [1, 2, 3, 4, 5]
 
 
 # Sorting a List in Descending Order (reverse=True)
-# numbers.sort(reverse=True)
-# print(numbers) 
+# number.sort(reverse=True)
+# print(number) 
  # Output: [5, 4, 3, 2, 1]
 
 # Reversing a List
-# numbers.reverse()
-# print(numbers)  
+number.reverse()
+# print(number)  
 # Output: [5, 4, 3, 2, 1]   
 
 #Copying a List
 list1 = [1, 2, 3]
 list2 = list1.copy()
 list2.append(4)
-# print(list1)  # Output: [1, 2, 3]
-# print(list2)  # Output: [1, 2, 3, 4]
+print(list1)  # Output: [1, 2, 3]
+print(list2)  # Output: [1, 2, 3, 4]
 
 #joining Lists
 list1 = [1, 2, 3]
